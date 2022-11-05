2d4ddd1873960cd336b0bfe58e97065877e959d4
Trevor <trevorgeek@gmail.com>
2022-11-05 15:19:06 +1300

    Add the code stubs

---
+++ NeuralMLP/Engine.h
@@ -0,0 +1,30 @@
+//--------------------------------------------------
+// Defines a basic engine for a vanilla C++ project.
+//
+// @author: Wild Boar
+//
+// @date: 2022-11-05
+//--------------------------------------------------
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+#include <NVLib/Logger.h>
+#include <NeuralMLPLib/ArgUtils.h>
+
+namespace NVL_App
+{
+	class Engine
+	{
+	private:
+		NVLib::Parameters * _parameters;
+		NVLib::Logger* _logger;
+	public:
+		Engine(NVLib::Logger* logger, NVLib::Parameters * parameters);
+		~Engine();
+
+		void Run();
+	};
+}
